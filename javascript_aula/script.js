15c4ecda10b6500e39cfea2062d2dba82bd4f46b
Igor Pires <97624371+igorpires000@users.noreply.github.com>
2022-09-14 17:54:21 -0300

    up

--- javascript_aula/script.js
+++ javascript_aula/script.js
@@ -1,19 +1,35 @@
-let n1 = document.querySelector('#num_1')
-let n2 = document.querySelector('#num_2')
-let resultado = document.querySelector('#res')
+//Aula 12 Operador Spread//
+//o spread são os 3 pontos
+//o spread pode ser usado também em objeto
+
+/*const jogador1 ={
+    nome:"Igor",
+    energia: 100,
+    vida: 3,
+    magia: 30
+}
+
+const jogador2 ={
+    nome:"Breno",
+    energia: 100,
+    vida: 5,
+    magia: 80
+}
+const jogador3 ={...jogador1, ...jogador2};
+
+console.log(jogador3)
+*/
+/*
+const soma =(v1,v2,v3) =>{
+    return v1+ v2+ v3;
+}
+let  valores = [1,5,4]
+
+console.log(soma(...valores))
+*/
+//sem o spread da undifined com ele 
+//o console consegue executar
 
 
-
-function somar(){
-     resultado.innerHTML = parseFloat(n1.value) + parseFloat(n2.value);  
-}
-function sub(){
-    resultado.innerHTML = parseFloat(n1.value) - parseFloat(n2.value);      
-}
-function multi(){
-    resultado.innerHTML = parseFloat(n1.value) * parseFloat(n2.value);      
-}
-function divi(){
-    resultado.innerHTML = parseFloat(n1.value) / parseFloat(n2.value);      
-}
+//Aula 13 If / If Else
 
