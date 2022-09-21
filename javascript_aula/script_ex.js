2e784196d39ecf34b100d48e8f14b31688314b5c
Igor Pires <97624371+igorpires000@users.noreply.github.com>
2022-09-21 17:23:52 -0300

    up

---
+++ javascript_aula/script_ex.js
@@ -0,0 +1,64 @@
+//Ex 1 Faça um algoritmo resultado da sua soma
+//que receba dois números e exiba-o
+
+/*function al(a,b){
+   return a+b;
+}
+console.log(al(5,5));
+*/
+//Ex 2 faça um algoritmo que receba dois números e ao final mostre a soma,
+// subtração, multiplicação e a divisão dos números lidos
+/*
+function soma(){
+    let a = 5;
+    let b = 5;
+    return a+b;
+}
+console.log(soma())
+
+function sub(){
+    let a = 5;
+    let b = 5;
+    return a-b;
+}
+console.log(sub())
+
+function mul(){
+    let a = 5;
+    let b = 5;
+    return a*b;
+}
+console.log(mul())
+
+function div(){
+    let a = 5;
+    let b = 5;
+    return a/b;
+}
+console.log(div())
+*/
+
+//Ex 3 Escrever um algoritmo para determinar o consumo médio de um automóvel sendo fornecida a 
+//distância total percorrida pelo automóvel e o total de combustível gasto
+
+/*function gas(){
+    let dis = 20;
+    let con = 1;
+    let soma = dis/con;
+        console.log(`A autonomia de consumo é: ${soma} Km/L`)
+}
+gas()
+*/
+//Ex 4 Escrever um algoritmo que leia o nome de um vendedor, o seu salário fixo e 
+//o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% 
+//de comissão sobre suas vendas efetuadas, informar o seu nome, o salário fixo e salário no final do mês
+
+/*function venda(){
+    let sal = 1000;
+    let ven = (15/100) * 30  * 30 ;
+    let valor = sal + ven;
+    console.log(`O salário dele neste mês mais as comissões é de : ${valor}`);
+
+}
+venda();
+*/
